b5b4c61f3276a1234368ca2a07a4e6a6cb9a02e1
Evgenia Gorbacheva <evgenia.gorbacheva@appodeal.com>
2025-08-22 14:28:01 +0200

    0.0.1

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ComposeApp",
-            url: "https://s3-us-west-1.amazonaws.com/appodeal-ios/GPConsentManager/SPM/ComposeApp-1.0.0.zip",
-            checksum: "b51d20da5ced396c0e0b840bc86e8498149d57effcc5886d8a8d9c7d99cd022c"
+            url: "https://s3-us-west-1.amazonaws.com/appodeal-ios/GPConsentManager/SPM/ComposeApp-0.0.1.zip",
+            checksum: "b837386de1d0e005c68fc13e8d6beedd9c8998120a9474e1452ada9b165aa0a9"
         )
     ]
 )
